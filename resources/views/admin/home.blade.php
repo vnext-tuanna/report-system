52c2805596a8465a5b18a9f83a3e73bc44551d82
vnext-sonnv <sonnv2@vnext.com.vn>
2021-11-05 16:50:23 +0700

    fix: Tmp

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.dashboard')
+
+@section('content')
+<div class="overview-boxes">
+    <div class="box">
+        <div class="right-side">
+            <div class="box-topic">Total Report</div>
+            <div class="number">40</div>
+        </div>
+        <i
+            style="font-size: 2rem; padding-top: 1rem"
+            class="fas fa-book"
+        ></i>
+    </div>
+    <div class="box">
+        <div class="right-side">
+            <div class="box-topic">Total Report</div>
+            <div class="number">40</div>
+        </div>
+        <i
+            style="font-size: 2rem; padding-top: 1rem"
+            class="fas fa-book"
+        ></i>
+    </div>
+    <div class="box">
+        <div class="right-side">
+            <div class="box-topic">Total Report</div>
+            <div class="number">40</div>
+        </div>
+        <i
+            style="font-size: 2rem; padding-top: 1rem"
+            class="fas fa-book"
+        ></i>
+    </div>
+    <div class="box">
+        <div class="right-side">
+            <div class="box-topic">Total Report</div>
+            <div class="number">40</div>
+        </div>
+        <i
+            style="font-size: 2rem; padding-top: 1rem"
+            class="fas fa-book"
+        ></i>
+    </div>
+    
+</div>
+@endsection
---
+++ resources/views/admin/request.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.layouts.dashboard') 
+
+@section('content')
+    <div class="container">
+        <table
+            id="example"
+            class="table table-striped table-bordered dt-responsive nowrap"
+            style="width: 100%"
+        >
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Full_name</th>
+                    <th>Content</th>
+                    <th>Type</th>
+                    <th>Time</th>
+                    <th>Request_to</th>
+                    <th>Option</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Tiger1</td>
+                    <td>Nixon</td>
+                    <td>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni illo quisquam in soluta. Veniam facere doloribus odit blanditiis suscipit beatae distinctio quasi minima laboriosam necessitatibus, natus recusandae illo voluptas.
+                      </p>
+                    <a href="# " id="myBtn" data-toggle="modal" data-target="#staticBackdrop">Chi tiết</a>
+                    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Content</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                            ...Contentttttttttt
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    </td>
+                    <td>Edinburgh</td>
+                    <td>19:03</td>
+                    <td>Son</td>
+                    <td>
+                        <a href="#">Deny</a>
+                        <a href="#">Approve</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
