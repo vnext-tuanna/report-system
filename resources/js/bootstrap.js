047c891646e2bf1449ca445abb03febdb064a0ef
vnext-sonnv <sonnv2@vnext.com.vn>
2021-11-05 17:39:12 +0700

    feat: Installbootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,13 @@
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+require('bootstrap');
+
+
+
 window._ = require('lodash');
 
 /**
@@ -5,6 +15,8 @@ window._ = require('lodash');
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+
+
 
 window.axios = require('axios');
 
